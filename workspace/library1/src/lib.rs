248c362dc868a824b1aef04e03fde42ecfe65bc0
Your Name <you@example.com>
2021-08-16 01:13:53 +0900

    Add test to library1

--- workspace/library1/src/lib.rs
+++ workspace/library1/src/lib.rs
@@ -1,1 +1,15 @@
 pub fn item() {}
+
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(greet("John Doe"), "Hello, John Doe!");
+    }
+}
